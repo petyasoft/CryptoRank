b6fc97a691bd9a2a526663ce95e753cacd139344
petyasoft <168456597+petyasoft@users.noreply.github.com>
2024-07-14 14:19:03 +0300

    Create config.py

---
+++ data/config.py
@@ -0,0 +1,32 @@
+# api id, hash
+API_ID = 11111111
+API_HASH = 'Your api hash'
+
+USE_TG_BOT = False # True if you want use tg, else False
+BOT_TOKEN = '283993:kdmioieiweikiokeocki4okew' # API TOKEN get in @BotFather
+CHAT_ID = '22803822' # Your telegram id
+
+# задержка между подключениями к аккаунтам
+ACC_DELAY = [10,15]
+
+# тип прокси
+PROXY_TYPE = "socks5" # http/socks5
+
+# папка с сессиями (не менять)
+WORKDIR = "sessions/"
+
+# использование прокси
+USE_PROXY = True # True/False
+
+# мини задержки
+MINI_SLEEP = [3,7] #[min,max]
+
+
+hello ='''              _                               __  _        
+ _ __    ___ | |_  _   _   __ _  ___   ___   / _|| |_  ___ 
+| '_ \  / _ \| __|| | | | / _` |/ __| / _ \ | |_ | __|/ __|
+| |_) ||  __/| |_ | |_| || (_| |\__ \| (_) ||  _|| |_ \__ \\
+| .__/  \___| \__| \__, | \__,_||___/ \___/ |_|   \__||___/
+|_|                |___/        
+
+'''
